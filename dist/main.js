06022fef2dce45518ee4906e527018095cc79e19
unknown <ignacioamaya04@gmail.com>
2025-11-30 13:33:42 -0300

    solucion de cors

--- dist/main.js
+++ dist/main.js
@@ -5,7 +5,8 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: process.env.FRONTEND_URL || 'http://localhost:3001',
+        origin: 'https://tp-final-83udg9es2-ayrtons-projects-e1e3b1b7.vercel.app',
+        credentials: true,
     });
     const port = process.env.PORT || 3000;
     await app.listen(port);
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Habilitar CORS solo
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
+    origin: 'https://tp-final-83udg9es2-ayrtons-projects-e1e3b1b7.vercel.app',
+    credentials: true, // permite enviar cookies y headers de autenticación
   });
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Server running on port ${port}`);
 }
+
 bootstrap();
