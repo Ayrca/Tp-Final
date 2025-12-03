718ff2cd41ea0f2414047a1f5be37f41caa135fe
unknown <ignacioamaya04@gmail.com>
2025-12-03 19:34:31 -0300

    cambios css admin

--- client/src/components/ManejoOficios.jsx
+++ client/src/components/ManejoOficios.jsx
@@ -199,13 +199,13 @@ const handleGuardar = (oficio) => {
               <td>
                 {editarOficio && editarOficio.idOficios === oficio.idOficios ? (
                   <div>
-                    <button onClick={() => handleGuardar(oficio)}>Guardar</button>
-                    <button onClick={() => setEditarOficio(null)}>Cancelar</button>
+                    <button className="btn-guardar" onClick={() => handleGuardar(oficio)}>Guardar</button>
+                    <button className="btn-cancelar" onClick={() => setEditarOficio(null)}>Cancelar</button>
                   </div>
                 ) : (
                   <div>
-                    <button onClick={() => handleEditar(oficio)}>Editar</button>
-                    <button onClick={() => handleBorrar(oficio)}>Borrar</button>
+                    <button className="btn-editar" onClick={() => handleEditar(oficio)}>Editar</button>
+                    <button className="btn-borrar" onClick={() => handleBorrar(oficio)}>Borrar</button>
                   </div>
                 )}
               </td>
